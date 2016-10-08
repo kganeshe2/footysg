8cb3b7473ef58782b77f9249ad2ec65fd55292f7
pandaxfly <pandaxfly@gmail.com>
2016-10-08 17:05:36 +0800

    Added create new Pitch functionality

--- imports/ui/pitch-list.js
+++ imports/ui/pitch-list.js
@@ -9,3 +9,29 @@ Template.pitchList.helpers({
 		return Pitches.find();
 	}
 });
+
+Template.pitchList.events({
+	'submit .new-pitch'(event){
+
+		event.preventDefault();
+		
+		// Get values 
+		var name, description, region;
+		name = event.target.pitchName.value;
+		description = event.target.pitchDescription.value;
+		region = event.target.pitchRegion.value;
+		
+		// Insert into db
+		Pitches.insert({
+			name: name,
+			description: description,
+			region: region,
+		});
+
+		// Dismiss modal
+		$('#new_pitch_form').modal('hide');
+	},
+});
+
+
+
