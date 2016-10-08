cc6cac236a6c25f58589ab967947ad543307b9ce
pandaxfly <pandaxfly@gmail.com>
2016-10-08 14:00:30 +0800

    restructured project
    Move all templates in separate UI folder

--- client/main.js
+++ client/main.js
@@ -1,5 +1,11 @@
 // import { Template } from 'meteor/templating';
 // import { ReactiveVar } from 'meteor/reactive-var';
 
-// import './main.html';
+// import routing rule from router.js
+import '../imports/ui/router.js';
 
+// import user views
+import '../imports/ui/landing.js';
+import '../imports/ui/home.js';
+import '../imports/ui/pitch-list.js';
+import '../imports/ui/pitch-info.js';
