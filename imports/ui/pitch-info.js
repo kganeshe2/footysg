e23921c0311aca255a8738e6b6eaff80181ab653
pandaxfly <pandaxfly@gmail.com>
2016-12-22 21:48:26 +0800

    add css for time fade in

--- imports/ui/pitch-info.js
+++ imports/ui/pitch-info.js
@@ -31,3 +31,37 @@ Template.pitchInfo.events({
 	}
 });
 
+//var minDate = $.fullCalendar.moment('2017-12-22');
+
+Template.monthCalendar.onRendered(function() {
+		var monthCalendar = $('#month-calendar').fullCalendar({
+			header: false,
+			firstDay: 1,
+			aspectRatio: 2,
+			dayRender: function(date, cell){
+    			/*if (date < minDate){
+		            $(cell).addClass('fc-other-month');
+		        }*/
+		    },
+			dayClick: function(date){
+				console.log('You selected ' + date.format());
+		        // change the day's background color just for fun
+		        $(this).css('background-color', 'red');
+
+		        // show the time calendar
+		        var timeCalendar = $('#time-calendar').fullCalendar({
+		        		header: false,
+		        		hiddenDays: [1],
+		        		aspectRatio: 1,
+		       		 });
+
+		        $('#time-calendar').animate({opacity: 1}, 500, function(){
+		        });
+
+		        
+		        $('#time-calendar thead').remove();
+		        $('#time-calendar .fc-today').removeClass('fc-today');
+			}
+	});
+		monthCalendar.fullCalendar( 'today' );
+});
