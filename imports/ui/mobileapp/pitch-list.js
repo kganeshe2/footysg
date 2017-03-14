848cf98436b786600ef0ad50bf2d186b1179b82a
pandaxfly <pandaxfly@gmail.com>
2017-03-14 23:19:21 +0800

    Update booking flow
    1. Changed URL to reflect new flow
    2. Show Parks list instead of pitch list
    3. Added new page: park detail
    4. Removed add new park

--- imports/ui/mobileapp/pitch-list.js
+++ imports/ui/mobileapp/pitch-list.js
@@ -1,28 +1,4 @@
-import { Pitches } from '/imports/api/pitches.js';
-
 import './pitch-list.html';
-
-Template.pitchList.events({
-  'submit .new-pitch'(event) {
-    // Prevent default function calls
-    event.preventDefault();
-
-    // Get values
-    const name = event.target.pitchName.value;
-    const description = event.target.pitchDescription.value;
-    const region = event.target.pitchRegion.value;
-
-    // Insert into db
-    Pitches.insert({
-      name: name,
-      description: description,
-      region: region,
-    });
-
-    // Dismiss modal
-    $('#new_pitch_form').modal('hide');
-  },
-});
 
 Template.pitchListItem.helpers({
   encodedName(name){
