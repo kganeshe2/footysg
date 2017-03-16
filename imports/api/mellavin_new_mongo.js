d662846546c4a022aec5ac3caed7c9a0f23a2661
pandaxfly <pandaxfly@gmail.com>
2017-03-16 22:17:51 +0800

    Enhanced cancel booking from myGames.
    1. enhanced cancel: only upcoming show cancel button

--- imports/api/mellavin_new_mongo.js
+++
@@ -1,4 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Regions = new Mongo.Collection('regions_mella_new_mongo');
-export const FutsalParks = new Mongo.Collection('futsalParks_mella_new_mongo');
